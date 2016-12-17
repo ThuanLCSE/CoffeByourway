055274b94ae22a0034ee4166a817b5b894a91c61
nvhungkt <ah.hung.ah@gmail.com>
2016-12-17 21:46:09 +0700

    update UI

--- client/components/home/HomePage.jsx
+++ client/components/home/HomePage.jsx
@@ -27,10 +27,15 @@ class HomePage extends React.Component{
   }
 
 signInModal(){
+    let customContentStyle = {
+        width: '25%',
+        maxWidth: 'none'
+      };
+    
     return(
       <Dialog
             title={(<p><b>Sign In</b></p>)}
-             modal={true}
+             modal={false}
              open={this.state.open}
              contentStyle={customContentStyle}
              onRequestClose={this.handleClose}
@@ -52,38 +57,20 @@ signInModal(){
   }
 
   render(){
-    let centerDiv = {
-      textAlign: 'center',
-      margin: '50 auto',
-      padding: '20% 0',
-      height: '100%'
-    }
     let fullHeight = {
         height: '100vh'
     }
-    let buttonDiv = {
-        marginBottom: '30px'
-    }
     let height100 = {
        height: '100%'
-    } 
-    let customContentStyle = {
-    width: '25%',
-    maxWidth: 'none'
-  };
-  
-
+    }
 
     return (
       <div className = "container-fluid" style={fullHeight}>
         <div className = "row" style={height100}>
-            <div className = "col-sm-6" style={centerDiv}>
-              <div className = "col-sm-12" style={buttonDiv}>
-                <RaisedButton  label="Sign In"  primary={true} onTouchTap={this.handleOpen}/> 
-              </div>
-              <div className = "col-sm-12">
-                <RaisedButton label="Order"  primary={true}/>
-              </div>
+            <div className = "col-sm-6"></div>
+            <div className = "col-sm-6" style={{overflow:'hidden'}}>
+                <img className = "TextCover" src="static/MyCollection.png" onClick={this.handleOpen}/>
+                <img className = "TextCover" src="static/CoffeeNow.png"/>
             </div>
         </div>
         {this.signInModal()}
