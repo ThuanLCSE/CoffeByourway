1b9c1157511bd9c50fbf368d771aadba198893f1
tiendat1997 <tiendatdeveloper1997@gmail.com>
2016-12-17 16:06:24 +0700

    sign in have dialog

--- client/components/home/HomePage.jsx
+++ client/components/home/HomePage.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
 
 
 
-const coverHome = {
-
-}
 
 class HomePage extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+        open: false,
+    }
+
+  this.handleOpen = this.handleOpen.bind(this);
+  this.handleClose = this.handleClose.bind(this);
   };
+
+  handleOpen() {
+    this.setState({open: true});
+  }
+  handleClose() {
+    this.setState({open: false});
+  }
 
 
   render(){
@@ -29,6 +42,27 @@ class HomePage extends React.Component{
     let height100 = {
        height: '100%'
     }
+    // const actions = [
+    //
+    //   <FlatButton
+    //     label="Cancel"
+    //     primary={true}
+    //     onTouchTap={this.handleClose}
+    //   />,
+    //   <FlatButton
+    //     label="Submit"
+    //     primary={true}
+    //     disabled={false}
+    //     keyboardFocused={true}
+    //     onTouchTap={this.handleClose}
+    //
+    //   />,
+    // ];
+    let customContentStyle = {
+    width: '25%',
+    maxWidth: 'none'
+  };
+
 
 
 
@@ -37,10 +71,31 @@ class HomePage extends React.Component{
         <div className = "row" style={height100}>
             <div className = "col-sm-6" style={centerDiv}>
               <div className = "col-sm-12" style={buttonDiv}>
-                <RaisedButton  label="Create New Coffee"/>
+                <RaisedButton  label="Sign In"  primary={true} onTouchTap={this.handleOpen}/>
+                <Dialog
+                    title={(<p><b>Sign In</b></p>)}
+                     modal={true}
+                     open={this.state.open}
+                     contentStyle={customContentStyle}
+                     onRequestClose={this.handleClose}
+                   >
+                   <TextField  floatingLabelText="Your email"
+                                   hintText="Your email"
+                                   fullWidth={true}
+                                /><br/>
+                    <TextField  type="password"
+                                   floatingLabelText="Your password"
+                                   hintText="Your password"
+                                   fullWidth={true}
+                                  /><br/>
+                    <div style={{margin: 'auto', width: '80%'}}>
+                       <RaisedButton label="Sign In" fullWidth={true} primary={true} onClick={this.handleClose}/>
+                   </div>
+                </Dialog>
+
               </div>
               <div className = "col-sm-12">
-                <RaisedButton label="Get Your Collection"/>
+                <RaisedButton label="Order"  primary={true}/>
               </div>
             </div>
         </div>
